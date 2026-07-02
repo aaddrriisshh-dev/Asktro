da297004caaf5abf97891a6b45591a33601fc975
Adrish Mullick <adrishmullick@Adrishs-MacBook-Air.local>
2026-07-02 18:40:10 +0530

    customer android build config (compileSdk 36, minSdk 24, ndk flag)

---
+++ apps/customer/android/build.gradle.kts
@@ -0,0 +1,30 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+
+subprojects {
+    afterEvaluate {
+        if (extensions.findByName("android") != null) {
+            (extensions.getByName("android") as com.android.build.gradle.BaseExtension).compileSdkVersion(36)
+        }
+    }
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
